e351a6680b9936efa18332d4776e18b3856d1f52
Gregory Granito <gregory.granito@gmail.com>
2015-03-07 14:06:14 -0500

    Js and Less compile and are served

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,19 +1,16 @@
 require.config({
     paths: {
     	'jquery' : 'lib/jquery.min',
-        'backbone': 'lib/backbone-min',
         'underscore' : 'lib/underscore-min'
     }
 });
 
 //MAIN BLOCK
-require(["backbone","jquery", "routers/main", "views/test"],function(Backbone, $, MainRouter, TestView) {
+require(["jquery"],function($) {
     $(document).ready(function(){
         $.ajaxSetup({
             contentType: "application/json"
         });
-    	$("body").append((new TestView()).el);
-		router = new MainRouter();
-    	Backbone.history.start({pushState: true});
+    	$("body").append("Working");
 	});
 });
