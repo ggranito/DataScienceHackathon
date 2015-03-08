10bb39a4011ae19e5944ce0b87a8838a3a538d31
Gregory Granito <gregory.granito@gmail.com>
2015-03-07 20:13:26 -0500

    Added mockup to play

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -14,6 +14,27 @@ require.config({
 
 //MAIN BLOCK
 require(["jquery", "angular"],function($, angular) {
+
+    var wg = function(){
+        $(".black").removeClass("black").addClass("gold");
+        $(".blue").removeClass("blue").addClass("white");
+        $(".black-text").removeClass("black-text").addClass("gold-text");
+        $(".blue-text").removeClass("blue-text").addClass("white-text");
+        setTimeout(bb, 5000);
+    }
+
+
+    var bb = function(){
+        $(".gold").removeClass("gold").addClass("black");
+        $(".white").removeClass("white").addClass("blue");
+        $(".gold-text").removeClass("gold-text").addClass("black-text");
+        $(".white-text").removeClass("white-text").addClass("blue-text");
+        setTimeout(wg, 5000);
+    };
+
+    wg();
+
+
     console.log(angular);
     var app = angular.module("main", []);
 
